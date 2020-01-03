60faf4bcb67d6edaf9c0fbfae74cdab1c63d36c2
Joseph Harkleroad <hark130@yahoo.com>
2020-01-02 21:00:38 -0600

    4-1: Added new GG_F_I function
    New function gets a list of entries from the database.
    Imma bout to change the name though...
    
    Changes to be committed:
    	modified:   modules/GG_File_IO.py

--- modules/GG_File_IO.py
+++ modules/GG_File_IO.py
@@ -7,3 +7,25 @@ def pick_tuple(filename):
         fileContent = inFile.read()
 
     return random.choice(fileContent.split("\n"))
+
+
+def pick_tuples(filename, numTuples):
+    """Returns a list of strings from a newline-delimited file"""
+    # LOCAL VARIABLES
+    listOfTuples = []
+
+    # BUILD LIST
+    # Open File
+    with open(filename, "r") as inFile:
+        # Read raw content
+        fileContent = inFile.read()
+
+    # Split Content
+    fileList = fileContent.split("\n")
+
+    # Choose List Entries
+    for _ in range(1, numTuples + 1):
+        listOfTuples.append(random.choice(fileList))
+
+    # DONE
+    return listOfTuples
