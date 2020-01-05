9f8dd1284d95d2226551b4b72095191432c650c1
Joseph Harkleroad <hark130@yahoo.com>
2020-01-05 00:59:46 -0600

    5-2: Fixed GG_Yaml
    THIS ISN'T C!  WHAT AM I DOING?
    
    Changes to be committed:
    	modified:   modules/GG_Yaml.py

--- modules/GG_Yaml.py
+++ modules/GG_Yaml.py
@@ -1,10 +1,10 @@
 import yaml
 
 
-GG_CITY_KEY "city"
-GG_CITY_NAME_KEY "name"
-GG_CITY_REGION_KEY "region"
-GG_CITY_RACE_KEY "ethnicity"
+GG_CITY_KEY = "city"
+GG_CITY_NAME_KEY = "name"
+GG_CITY_REGION_KEY = "region"
+GG_CITY_RACE_KEY = "ethnicity"
 
 
 def parse_yaml(filename):
