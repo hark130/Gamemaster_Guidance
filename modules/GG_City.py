ade1d8ed6d5b3d7e5f013f558b5bf03f27ff39ac
Joseph Harkleroad <hark130@yahoo.com>
2020-01-08 20:16:26 -0600

    5-3: GG_City inital commit
    I'm still working on the ctor
    Interim commit
    
    Changes to be committed:
    	new file:   modules/GG_City.py

---
+++ modules/GG_City.py
@@ -0,0 +1,40 @@
+import GG_City
+
+
+def get_key_value(theDict, theKey):
+    """Returns the key value as a float, 0.0 on Exception"""
+    try:
+        theValue = float(theDict[theKey])
+    except Exception:
+        theValue = float(0.0)
+
+    return theValue
+
+
+class GG_City:
+
+    def __init__(self, cityDict):
+        """Class constructor"""
+        detailsDict = cityDict[GG_CITY_KEY]
+        cityEthnicity = detailsDict[GG_CITY_RACE_KEY]
+
+        self.name = detailsDict[GG_CITY_NAME_KEY]
+        self.region = detailsDict[GG_CITY_REGION_KEY]
+        self.dwarfPercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_DWARF)
+        self.elfPercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_ELF)
+        self.gnomePercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_GNOME)
+        self.goblinPercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_GOBLIN)
+        self.halflingPercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_HALFLING)
+        self.garundiPercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_GARUNDI)
+        self.keleshitePercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_KELESHITE)
+        self.kellidPercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_KELLID)
+        self.mwangiPercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_MWANGI)
+        self.nidalesePercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_NIDALESE)
+        self.shoantiPercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_SHOANTI)
+        self.taldanPercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_TALDAN)
+        self.tianPercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_TIAN)
+        self.ulfenPercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_ULFEN)
+        self.varisianPercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_VARISIAN)
+        self.vudraniPercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_VUDRANI)
+        self.halfElfPercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_HALF_ELF)
+        self.halfOrcPercent = get_key_value(cityEthnicity, GG_City.GG_CITY_RACE_HALF_ORC)
