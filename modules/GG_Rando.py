95de423a71d5feb9ea5368c2111cacfffd2fa4b2
Joseph Harkleroad <hark130@yahoo.com>
2020-01-09 23:05:47 -0600

    5-3: Defined rand_float() in GG_Rando
    Complete and functional
    
    Changes to be committed:
    	modified:   modules/GG_Rando.py

--- modules/GG_Rando.py
+++ modules/GG_Rando.py
@@ -4,3 +4,8 @@ import random
 def rand_percent():
     """Return an integer between 1 and 100"""
     return int(100 * random.uniform(0.01, 1.0))
+
+
+def rand_float(start, stop):
+    """Return a random float between start and stop"""
+    return random.uniform(start, stop)
