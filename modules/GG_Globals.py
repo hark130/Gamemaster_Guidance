9f6b8daae1fc8a64e9cd242f36233fd12b0f6e94
Joseph Harkleroad <hark130@yahoo.com>
2020-01-13 20:39:18 -0600

    5-4: GG_Globals initial commit
    I started extricating static lists into a separate module so multiple
    classes can use them
    
    Changes to be committed:
    	new file:   modules/GG_Globals.py

---
+++ modules/GG_Globals.py
@@ -0,0 +1,2 @@
+ancestryList = ["Dwarf", "Elf", "Gnome", "Goblin", "Halfling", "Human", "Half-Elf", "Half-Orc"]
+humanEthnicityList = ["Garundi", "Keleshite", "Kellid", "Mwangi", "Nidalese", "Shoanti", "Taldan", "Tian", "Ulfen", "Varisian", "Vudrani"]  # User Story 8. Nidalese
