4ac21694c91b03bc749ad8a63688ffe88374a80d
Joseph Harkleroad <hark130@yahoo.com>
2020-01-14 20:12:16 -0600

    5-4: Setting locale in main()
    Set the locale to easily translate string-based integers that may have
    commas in them
    
    Changes to be committed:
    	modified:   Gamemaster_Guidance.py

--- Gamemaster_Guidance.py
+++ Gamemaster_Guidance.py
@@ -3,12 +3,15 @@ from modules.GG_Menu import menu
 from modules.GG_Yaml import parse_yaml
 
 
+import locale
 import sys
 
 
 def main(argList):
     # LOCAL VARIABLES
     cityDict = None
+
+    locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
 
     parsedArgs = parse_arguments(argList)
     if "cityfile" in parsedArgs.keys():
