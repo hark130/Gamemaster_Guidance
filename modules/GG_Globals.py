14a41e1f4fbec8d8363a071c21ae3066108c552d
Joseph Harkleroad <hark130@yahoo.com>
2020-01-16 20:16:32 -0600

    5-4: Added citySizeLimits to GG_Globals
    This tuple will establish the lower and upper end size limit for
    populations randomized by Gamemaster_Guidance
    
    Changes to be committed:
    	modified:   modules/GG_Globals.py

--- modules/GG_Globals.py
+++ modules/GG_Globals.py
@@ -1,2 +1,3 @@
 ancestryList = ["Dwarf", "Elf", "Gnome", "Goblin", "Halfling", "Human", "Half-Elf", "Half-Orc"]
 humanEthnicityList = ["Garundi", "Keleshite", "Kellid", "Mwangi", "Nidalese", "Shoanti", "Taldan", "Tian", "Ulfen", "Varisian", "Vudrani"]  # User Story 8. Nidalese
+citySizeLimits = (5, 200000)  # (Min, Max)
