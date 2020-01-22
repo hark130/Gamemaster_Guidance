179e20b1ce8a526d885f126d4f17bbeae61f40a1
Joseph Harkleroad <hark130@yahoo.com>
2020-01-21 19:39:02 -0600

    11-2: Added city modifier list to GG_Globals
    Added this to save some code when updating and then iterating over the
    modifier dictionary in GG_City.
    
    Also, reordered the global variables here in alphabetical order.
    
    Changes to be committed:
    	modified:   modules/GG_Globals.py

--- modules/GG_Globals.py
+++ modules/GG_Globals.py
@@ -1,3 +1,4 @@
 ancestryList = ["Dwarf", "Elf", "Gnome", "Goblin", "Halfling", "Human", "Half-Elf", "Half-Orc"]
-humanEthnicityList = ["Garundi", "Keleshite", "Kellid", "Mwangi", "Nidalese", "Shoanti", "Taldan", "Tian", "Ulfen", "Varisian", "Vudrani"]  # User Story 8. Nidalese
+cityModifierList = ["Corruption", "Crime", "Economy", "Law", "Lore", "Society"]
 citySizeLimits = (5, 200000)  # (Min, Max)
+humanEthnicityList = ["Garundi", "Keleshite", "Kellid", "Mwangi", "Nidalese", "Shoanti", "Taldan", "Tian", "Ulfen", "Varisian", "Vudrani"]  # User Story 8. Nidalese
