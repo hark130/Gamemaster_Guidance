9f939e7e3612235927b030da9266b53d9ef898d4
hark130 <hark130@yahoo.com>
2020-02-09 23:06:52 -0600

    2-3: Still defining GG_Bounty
    Fixed some BUGs
    Updated some 'print' methods
    Pausing here to finish adding functionality to GG_City to better support
    randomizing bounties
    
    Changes to be committed:
    	modified:   modules/GG_Bounty.py
    
    Changes not staged for commit:
    	modified:   modules/GG_City.py

--- modules/GG_Bounty.py
+++ modules/GG_Bounty.py
@@ -1,5 +1,5 @@
 # Standard Imports
-
+import random
 
 # Third Party Imports
 
@@ -20,17 +20,39 @@ class GG_Bounty(GG_Character):
         super().__init__(race, sex, numTraits, cityObject)
 
         # GG_Bounty
+        self._reward = None  # Bounty reward in gp
+        self._wanted_status = None  # WANTED: Dead or Alive
+        self._class = None  # Bounty's character class
+        self._level = None  # Bounty's character level
+
+        self._create_bounty()
+
+    def _create_bounty(self):
+        # Level
+        (self._class, self._level) = self.cityObj.rando_npc_class_level()
+        # Wanted Status
+        self._rando_wanted_status()
+        # Complications
+        self._rando_reward()
+
+    def _rando_reward(self):
         self._reward = 0
-        self._wanted_status = None
+
+    def _rando_wanted_status(self):
+        self._wanted_status = random.choice(self.supportedStates)
 
     def print_public_details(self):
         print_header("Public Details")
         self.print_name()
         self.print_race()
         self.print_sex()
+        self.print_reward()
+        self.print_wanted_status()
 
     def print_private_details(self):
         print_header("Private Details")
+        self.print_bounty_class()
+        self.print_bounty_level()
 
     def print_gm_details(self):
         print_header("GM Details")
@@ -45,8 +67,16 @@ class GG_Bounty(GG_Character):
 
     def print_reward(self):
         """Print the bounty's reward"""
-        self._print_something("Reward:", self._reward)
+        self._print_something("Reward:", str(self._reward))
 
     def print_wanted_status(self):
         """Print the bounty's wanted status"""
         self._print_something("Wanted:", self._wanted_status)
+
+    def print_bounty_class(self):
+        """Print the bounty's character class"""
+        self._print_something("Class:", self._class)
+
+    def print_bounty_level(self):
+        """Print the bounty's character level"""
+        self._print_something("Level:", self._level)
