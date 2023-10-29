49ddd9c240d2dc93dd319b616d819838c0035613
hark130 <hark130@yahoo.com>
2023-10-29 16:28:33 -0500

    GAGU-1: Fixed and polished main module
    Changes to be committed:
    	modified:   gamemaster_guidance/__main__.py
    
    Changes not staged for commit:
    	modified:   gamemaster_guidance/gg_ancestry.py
    	modified:   gamemaster_guidance/gg_bounty.py
    	modified:   gamemaster_guidance/gg_character.py
    	modified:   gamemaster_guidance/gg_city.py

--- gamemaster_guidance/__main__.py
+++ gamemaster_guidance/__main__.py
@@ -1,22 +1,26 @@
-from modules.GG_Arguments import parse_arguments
-from modules.GG_Menu import menu
-from modules.GG_Yaml import parse_yaml
+"""Defines the entry level function for this package."""
 
-
+# Standard
 import locale
 import sys
+# Third Party
+# Local
+from gamemaster_guidance.gg_arguments import parse_arguments
+from gamemaster_guidance.gg_menu import menu
+from gamemaster_guidance.gg_yaml import parse_yaml
 
 
-def main(argList):
+def main(arg_list):
+    """Entry level function for this package."""
     # LOCAL VARIABLES
-    cityDict = None
+    city_dict = None
 
     locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
 
-    parsedArgs = parse_arguments(argList)
-    if "cityfile" in parsedArgs.keys():
-        cityDict = parse_yaml(parsedArgs["cityfile"])
-    menu(cityDict)
+    parsed_args = parse_arguments(arg_list)
+    if 'cityfile' in parsed_args:
+        city_dict = parse_yaml(parsed_args['cityfile'])
+    menu(city_dict)
 
 
 if __name__ == "__main__":
