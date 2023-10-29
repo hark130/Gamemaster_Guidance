be9ab8008804c03a692cb8db7bc9ea628e86bc60
hark130 <hark130@yahoo.com>
2023-10-29 18:30:41 -0500

    GAGU-13: Updated the package's entry point
    Entry point updated to use the new argument parser.
    Everything appears to be working.
    
    Changes to be committed:
    	modified:   gamemaster_guidance/__main__.py

--- gamemaster_guidance/__main__.py
+++ gamemaster_guidance/__main__.py
@@ -10,18 +10,18 @@ from gamemaster_guidance.gg_menu import menu
 from gamemaster_guidance.gg_yaml import parse_yaml
 
 
-def main(arg_list):
+def main():
     """Entry level function for this package."""
     # LOCAL VARIABLES
     city_dict = None
 
     locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
 
-    parsed_args = parse_arguments(arg_list)
+    parsed_args = parse_arguments()
     if 'cityfile' in parsed_args:
         city_dict = parse_yaml(parsed_args['cityfile'])
     menu(city_dict)
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
