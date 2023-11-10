1a3c42c5d9bf6477e3d61f9f0e19e97f72f4c0b7
hark130 <hark130@yahoo.com>
2023-11-10 00:26:10 -0600

    GAGU-14: Added guildfile support to main()
    Added support for the new guildfile config to the entry level function.
    
    Changes to be committed:
    	modified:   gamemaster_guidance/__main__.py
    
    Changes not staged for commit:
    	modified:   gamemaster_guidance/gg_arguments.py
    	modified:   gamemaster_guidance/gg_globals.py
    	modified:   gamemaster_guidance/gg_menu.py
    
    Untracked files:
    	configs/Guild-test_guild1.yml
    	gamemaster_guidance/gg_guild.py
    	gamemaster_guidance/gg_job.py
    	gamemaster_guidance/gg_misc.py

--- gamemaster_guidance/__main__.py
+++ gamemaster_guidance/__main__.py
@@ -4,7 +4,7 @@
 import locale
 # Third Party
 # Local
-from gamemaster_guidance.gg_arguments import parse_arguments
+from gamemaster_guidance.gg_arguments import ARG_DICT_KEY_CITY, ARG_DICT_KEY_GANG, parse_arguments
 from gamemaster_guidance.gg_menu import menu
 from gamemaster_guidance.gg_yaml import parse_yaml
 
@@ -12,14 +12,17 @@ from gamemaster_guidance.gg_yaml import parse_yaml
 def main():
     """Entry level function for this package."""
     # LOCAL VARIABLES
-    city_dict = None
+    city_dict = None   # Dictionary parsed from --cityfile yml
+    guild_dict = None  # Dictionary parsed from --guildfile yml
 
     locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
 
     parsed_args = parse_arguments()
-    if 'cityfile' in parsed_args:
-        city_dict = parse_yaml(parsed_args['cityfile'])
-    menu(city_dict)
+    if ARG_DICT_KEY_CITY in parsed_args:
+        city_dict = parse_yaml(parsed_args[ARG_DICT_KEY_CITY])
+    if ARG_DICT_KEY_GANG in parsed_args:
+        guild_dict = parse_yaml(parsed_args[ARG_DICT_KEY_GANG])
+    menu(city_dict, guild_dict)
 
 
 if __name__ == "__main__":
