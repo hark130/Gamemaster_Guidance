71351a384378f83e0ee3cd724e6986b6efd54433
hark130 <hark130@yahoo.com>
2023-11-10 00:32:20 -0600

    GAGU-14: Extricated functionality into new gg_misc
    Initial commit
    The refactor isn't done yet though
    I haven't updated function names for the callers/imports
    
    Changes to be committed:
    	new file:   gamemaster_guidance/gg_misc.py
    
    Untracked files:
    	configs/Guild-test_guild1.yml

---
+++ gamemaster_guidance/gg_misc.py
@@ -0,0 +1,84 @@
+"""Defines miscellaneous functions for the package."""
+
+# Standard Imports
+from typing import Any, List
+# Third Party Imports
+# Local Imports
+
+
+def print_header(header: str) -> None:
+    """Print a header surrounded by a standardized banner."""
+    # LOCAL VARIABLES
+    banner = '-' * len(header)
+
+    print(banner)
+    print(header)
+    print(banner)
+
+
+def print_rjust_list(str_list: List[str]) -> None:
+    """Print a list of strings, right-justified to the width of the longest string."""
+    # LOCAL VARIABLES
+    longest_str = 0  # Length of the longest string in str_list
+
+    # INPUT VALIDATION
+    if not isinstance(str_list, list):
+        raise TypeError('The str_list argument must be of type list')
+    for str_list_entry in str_list:
+        if not isinstance(str_list_entry, str):
+            raise TypeError('A non-string was found in str_list')
+        if len(str_list_entry) > longest_str:
+            longest_str = len(str_list_entry)
+
+    # PRINT IT
+    for str_list_entry in str_list:
+        print(f"{(longest_str - len(str_list_entry)) * ' '}{str_list_entry}")
+
+
+def validate_num(value: int, name: str) -> None:
+    """Validate value is an integer between 1 and 100, inclusive.
+
+    Args:
+        value: Integer to test.
+        name: Argument name being validated.  Used to format exception messages.
+    """
+    # INPUT VALIDATION
+    if not isinstance(value, int):
+        raise TypeError(f'The {name} must be of type int')
+    _validate_percent(value=value, name=name)
+
+
+def validate_percent(value: Any, name: str, can_be_zero: bool = False) -> None:
+    """Validate value is an integer or float on an inclusive range of 1 to 100.
+
+    Args:
+        value: Value to test as a percentage.
+        name: Argument name being validated.  Used to format exception messages.
+    """
+    # INPUT VALIDATION
+    if isinstance(value, int) or isinstance(value, float):
+        if value == 0 and can_be_zero:
+            pass
+        elif value < 1:
+            raise ValueError(f'The {name} may not be less than 1')
+        if value > 100:
+            raise ValueError(f'The {name} may not be more than 100')
+    else:
+        raise TypeError(f'The {name} must be of type int or float')
+
+
+def validate_scale(value: Any, name: str) -> None:
+    """Validate value is an integer or float on an inclusive scale of 1 to 10.
+
+    Args:
+        value: Value to test as a number on a scale of 1 to 10.
+        name: Argument name being validated.  Used to format exception messages.
+    """
+    # INPUT VALIDATION
+    if isinstance(value, int) or isinstance(value, float):
+        if value < 1:
+            raise ValueError(f'The {name} may not be less than 1')
+        if value > 10:
+            raise ValueError(f'The {name} may not be more than 10')
+    else:
+        raise TypeError(f'The {name} must be of type int or float')
